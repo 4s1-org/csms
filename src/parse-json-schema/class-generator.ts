cc26c698da395b9e8f61bf01996a188f36dfb186
Steffen <steffen@4s1.de>
2020-12-12 14:42:49 +0100

    Keine Duplikate

--- src/parse-json-schema/class-generator.ts
+++ src/parse-json-schema/class-generator.ts
@@ -88,14 +88,15 @@ export class ClassGenerator {
         content.push(`import { ${classValidatorImports.join(", ")} } from 'class-validator'`)
       }
       content.push(`import { ApiProperty } from '@nestjs/swagger'`)
+
       // DTOs importieren
       for (const item of ele.items) {
         if (item.type.endsWith("EnumType")) {
           const type = item.type.substr(0, item.type.length - 8)
-          content.push(`import { ${type}Enum } from '../enums/${this.filenameFormatter(type)}.enum'`)
+          this.pushIfNotExists(content, `import { ${type}Enum } from '../enums/${this.filenameFormatter(type)}.enum'`)
         } else if (item.type.endsWith("Type")) {
           const type = item.type.substr(0, item.type.length - 4)
-          content.push(`import { ${type}Dto } from './${this.filenameFormatter(type)}.dto'`)
+          this.pushIfNotExists(content, `import { ${type}Dto } from './${this.filenameFormatter(type)}.dto'`)
         }
       }
       content.push(``)
@@ -144,6 +145,12 @@ export class ClassGenerator {
       content.push(``)
 
       fs.writeFileSync(file, content.join("\n"))
+    }
+  }
+
+  private pushIfNotExists(content: string[], text: string): void {
+    if (content.indexOf(text) === -1) {
+      content.push(text)
     }
   }
 
