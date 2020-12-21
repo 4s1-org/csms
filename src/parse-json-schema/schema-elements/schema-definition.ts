47767bac1c972de3e5e5e91a7e9d894bd33cba35
Steffen <yellowgarbagebag@4s1.de>
2020-12-21 11:08:40 +0100

    Backup

--- src/parse-json-schema/schema-elements/schema-definition.ts
+++ src/parse-json-schema/schema-elements/schema-definition.ts
@@ -1,8 +1,9 @@
 import { ClassGenerator } from "../class-generator"
 import { IKeyValue } from "../i-key-value"
+import { ClassSkeleton } from "../skeletons/class-skeleton"
 import { EnumSkeleton } from "../skeletons/enum-skeleton"
 import { Validatable } from "../validatable"
-import { SchemaDefinitionProperty, SchemaDefinitionPropertyItem } from "./schema-definition-property"
+import { SchemaDefinitionProperty } from "./schema-definition-property"
 
 interface Foo {
   javaType: string
@@ -44,25 +45,27 @@ export class SchemaDefinition extends Validatable<Foo> {
       const name = this.key.substr(0, this.key.length - 8)
       const skeleton = new EnumSkeleton(name, this.data.enum)
       skeleton.comment = this.data.description
-      ClassGenerator.instance.addEnumNew(skeleton)
-
-
-      ClassGenerator.instance.addEnum(this.key.substr(0, this.key.length - 8), this.data.enum, this.data.description)
+      ClassGenerator.instance.addEnum(skeleton)
     } else if (this.data.type === "object") {
       if (!this.data.properties) {
         throw new Error(`${this.key}: I thought it was a custom type`)
       }
       // const skeleton = new ClassSkeleton(this.key)
 
-      const items: SchemaDefinitionPropertyItem[] = []
+      // Remove "Type"
+      const name = this.key.substr(0, this.key.length - 4)
+      const skeleton = new ClassSkeleton(name, true)
+      skeleton.comment = this.data.description
+
       const required = this.data.required || []
       for (const key in this.data.properties) {
-        const item = new SchemaDefinitionProperty(key, this.data.properties[key])
+        const prop = this.data.properties[key]
+        const propSkeleton = skeleton.addProperty(key, required.includes(key))
+
+        const item = new SchemaDefinitionProperty(key, prop, propSkeleton)
         item.init()
-        item.Item.isRequired = required.includes(key)
-        items.push(item.Item)
       }
-      ClassGenerator.instance.addDto(this.data.javaType, this.data.description, items)
+      ClassGenerator.instance.addClass(skeleton)
     } else {
       throw new Error(`${this.key}: Unknown Type: ${this.data.type}`)
     }
---
+++ src/parse-json-schema/skeletons/property-skeleton.ts
@@ -0,0 +1,87 @@
+import { SkeletonBase } from "./skeleton-base"
+
+export class PropertySkeleton extends SkeletonBase {
+  private _annotations: string[] = []
+  private _type: string | undefined
+
+  public constructor(
+    name: string,
+    public readonly isRequired: boolean
+  ) {
+    super(name)
+
+    this._annotations.push(`@ApiProperty()`)
+
+    if (isRequired) {
+      this.addImportClassValidatior("IsNotEmpty")
+      this._annotations.push("@IsNotEmpty()")
+    } else {
+      this.addImportClassValidatior("IsOptional")
+      this._annotations.push("@IsOptional()")
+    }
+  }
+
+  public get type(): string | undefined {
+    return this._type;
+  }
+
+  public setIsEnum(params: string): void {
+    this.addImportClassValidatior("IsEnum")
+    this._annotations.push(`@IsEnum(${params})`)
+    this._type = params
+  }
+
+  public setIsString(): void {
+    this.addImportClassValidatior("IsString")
+    this._annotations.push(`@IsString()`)
+    this._type = "string"
+  }
+
+  public setIsNumber(): void {
+    this.addImportClassValidatior("IsNumber")
+    this._annotations.push(`@IsNumber()`)
+    this._type = "number"
+  }
+
+  public setIsInteger(): void {
+    this.addImportClassValidatior("IsInt")
+    this._annotations.push(`@IsInt()`)
+    this._type = "number"
+  }
+
+  public setIsCustomType(type: string): void {
+    this._type = "type"
+  }
+
+  /**
+   * @deprecated Don't use
+   */
+  public setIsAny(): void {
+    this._type = "any"
+  }
+
+  public setIsBoolean(): void {
+    this.addImportClassValidatior("IsBoolean")
+    this._annotations.push(`@IsBoolean()`)
+    this._type = "boolean"
+  }
+
+  public setLength(min: number, max: number): void {
+    this.addImportClassValidatior("Length")
+    this._annotations.push(`@Length(${min}, ${max})`)
+  }
+
+  public toString(): string[] {
+    const result: string[] = []
+    if (this.comment) {
+      result.push("  /**")
+      result.push(`   * ${this.comment}`)
+      result.push("   */")
+    }
+    for (const annotation of this._annotations) {
+      result.push(`  ${annotation}`)
+    }
+    result.push(`  public ${this.name}${this.isRequired ? "" : "!"}: ${this._type}`)
+    return result
+  }
+}
