d8eb2ed4b7eb06b8a03d06f621336687229f3f2e
Steffen <steffen@4s1.de>
2020-12-23 10:08:27 +0100

    Kommentare

--- src/parse-json-schema/generated/enumerations/operational-status.enum.ts
+++ src/parse-json-schema/generated/enumerations/operational-status.enum.ts
@@ -1,9 +1,11 @@
 // THIS FILE IS AUTO-GENERATED. DO NOT CHANGE IT!
 
 /**
- * This contains the type of availability change that the Charging Station should perform.
+ * Requested availability change.
  */
 export enum OperationalStatusEnum {
+  /** Charging Station is not available for charging. */
   Inoperative = "Inoperative",
+  /** Charging Station is available for charging. */
   Operative = "Operative",
 }
