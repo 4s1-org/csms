c2248fad9c76b085c6ed223b246e3429403ca87d
Steffen <steffen@4s1.de>
2020-12-23 10:22:20 +0100

    Kommentare

---
+++ src/parse-json-schema/comments/enumerations/ResetEnumType.ts
@@ -0,0 +1,13 @@
+import { enumCommentType } from "../enum-comments"
+
+export const ResetEnumType: enumCommentType = {
+  name: "ResetEnumType",
+  description: "Type of reset requested.",
+  values: [{
+    value: "Immediate",
+    description: "Immediate reset of the Charging Station."
+  }, {
+    value: "OnIdle",
+    description: "Delay reset until no more transactions are active."
+  }]
+}
