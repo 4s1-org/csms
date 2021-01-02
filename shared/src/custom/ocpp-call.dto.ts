b8e3482fa085e298c8a9ad81dcf40a874c031b32
Steffen <steffen@4s1.de>
2021-01-02 12:05:27 +0100

    Call DTOs überarbeitet

--- shared/src/custom/ocpp-call.dto.ts
+++ shared/src/custom/ocpp-call.dto.ts
@@ -1,44 +1,51 @@
 import { ApiProperty } from "@nestjs/swagger"
-import { IsEnum, IsInt, IsNotEmpty, IsOptional, IsPositive, IsString, ValidateNested } from "class-validator"
+import { IsEnum, IsInt, IsNotEmpty, IsOptional, IsPositive, IsString, MaxLength, ValidateNested } from "class-validator"
 import { OcppMessageEnum } from "../ocpp-message.enum"
 import { OcppMessageTypeIdEnum } from "./ocpp-message-type-id.enum"
-import { JSONObject } from "./types"
 
 export class OcppCallDto {
   constructor(
-    messageTypeId: OcppMessageTypeIdEnum.Call,
     messageId: string,
     action: OcppMessageEnum,
-    payload: JSONObject | any,
+    payload: unknown,
   ) {
-    this.messageTypeId = messageTypeId
+    this.messageTypeId = OcppMessageTypeIdEnum.Call
     this.messageId = messageId
     this.action = action
     this.payload = payload
   }
 
+  // ToDo: Fixer Wert setzen
   @ApiProperty()
   @IsNotEmpty()
   @IsInt()
   @IsPositive()
+  /** This is a Message Type Number which is used to identify the type of the message. */
   public messageTypeId: OcppMessageTypeIdEnum.Call
 
   @ApiProperty()
   @IsNotEmpty()
   @IsString()
+  @MaxLength(36)
+  /** This is a unique identifier that will be used to match request and result */
   public messageId: string
 
   @ApiProperty()
   @IsNotEmpty()
   @IsEnum(OcppMessageEnum)
+  /** The name of the remote procedure or action. This field SHALL contain a case-sensitive string.
+    * The field SHALL contain the OCPP Message name without the "Request" suffix. For example: For
+    * a "BootNotificationRequest", this field shall be set to "BootNotification".
+    */
   public action: OcppMessageEnum
 
   @ApiProperty()
-  @IsOptional()
+  @IsNotEmpty()
   @ValidateNested()
-  public payload: any
+  /** JSON Payload of the action. */
+  public payload: unknown
 
-  public toMessage(): [number, string, string, any] {
+  public toMessage(): [number, string, string, unknown] {
     return [this.messageTypeId, this.messageId, this.action, this.payload]
   }
 }
