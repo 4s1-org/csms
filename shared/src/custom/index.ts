baf8d622d6870bce96250f0ccef6756e1fba0e15
Steffen <yellowgarbagebag@4s1.de>
2021-01-03 12:00:51 +0100

    Singular

--- shared/src/custom/index.ts
+++ shared/src/custom/index.ts
@@ -1,5 +1,5 @@
 export { OcppCallErrorDto } from "./ocpp-call-error.dto"
 export { OcppCallResultDto } from "./ocpp-call-result.dto"
 export { OcppCallDto } from "./ocpp-call.dto"
-export { OcppErrorCodes } from "./ocpp-error-codes"
+export { OcppErrorCode } from "./ocpp-error-code"
 export { OcppMessageTypeIdEnum } from "./ocpp-message-type-id.enum"
