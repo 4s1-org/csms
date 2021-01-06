453a740072ea185e447fb2573caf193928b884b4
Steffen <yellowgarbagebag@4s1.de>
2021-01-06 17:06:11 +0100

    Liste der ChargingStations im Server erstellt

--- backend/src/csms/csms.gateway.ts
+++ backend/src/csms/csms.gateway.ts
@@ -1,4 +1,4 @@
-import { Logger, UseFilters, UsePipes, ValidationPipe } from '@nestjs/common'
+import { Logger, UseFilters } from '@nestjs/common'
 import {
   MessageBody,
   SubscribeMessage,
@@ -7,6 +7,7 @@ import {
   OnGatewayConnection,
   OnGatewayInit,
   OnGatewayDisconnect,
+  ConnectedSocket,
 } from '@nestjs/websockets'
 import { Server, Socket } from 'socket.io'
 import {
@@ -18,21 +19,33 @@ import {
   BootNotificationRequestDto,
   OcppErrorCode,
   IResponseMessage,
-  OcppCallErrorDto,
+  OcppMessageTypeIdEnum,
 } from '@yellowgarbagebag/csms-shared'
-import { OcppCallValidationPipe } from './ocpp-call-validation.pipe'
 import { AllWsExceptionsFilter } from '../all-ws-exceptions.filter'
 import { plainToClass } from 'class-transformer'
 import { validate } from 'class-validator'
 import { OcppWsException } from './ocpp-exception'
+import { IncomingMessage } from 'http'
+import { ChargingStation } from './charging-station'
 
 @WebSocketGateway({ path: '/ocpp/2.0.1' })
 @UseFilters(new AllWsExceptionsFilter())
 export class CsmsGateway implements OnGatewayInit, OnGatewayDisconnect, OnGatewayConnection {
+  private logger: Logger = new Logger('CsmsGateway')
+
+  private chargingStations: ChargingStation[] = []
+
+  private getChargingStation(uniqueIdentifier: string): ChargingStation {
+    let chargingStation = this.chargingStations.find((x) => x.uniqueIdentifier === uniqueIdentifier)
+    if (!chargingStation) {
+      chargingStation = new ChargingStation(uniqueIdentifier)
+      this.chargingStations.push(chargingStation)
+    }
+    return chargingStation
+  }
+
   @WebSocketServer()
   private server!: Server
-
-  private logger: Logger = new Logger('CsmsGateway')
 
   public afterInit(): void {
     this.logger.log('Websocket is available')
@@ -44,13 +57,57 @@ export class CsmsGateway implements OnGatewayInit, OnGatewayDisconnect, OnGatewa
 
   public async handleConnection(client: Socket): Promise<void> {
     this.logger.log(`Client connected: ${client.id}`)
+    const req: IncomingMessage = client.request as IncomingMessage
+    const parts = req.url?.split('/')
+    if (parts && parts.length >= 4) {
+      const chargingStation = this.getChargingStation(parts[3])
+      chargingStation.socketId = client.id
+    } else {
+      this.logger.log(`Client URL is invalid "${req.url}"`)
+      client.disconnect()
+    }
   }
 
-  @UsePipes(new OcppCallValidationPipe(), new ValidationPipe())
-  @SubscribeMessage('ocpp')
-  async ocppCommand(@MessageBody() ocppCall: OcppCallDto): Promise<[number, string, IResponseMessage] | any> {
-    let response: IResponseMessage
+  private async convertInputToOcppCall(data: unknown): Promise<OcppCallDto> {
+    if (!data) {
+      throw new OcppWsException(OcppErrorCode.RpcFrameworkError, 'Invalid data format received')
+    }
+    if (!Array.isArray(data)) {
+      throw new OcppWsException(OcppErrorCode.RpcFrameworkError, 'No array received')
+    }
+    if (data.length !== 4) {
+      throw new OcppWsException(OcppErrorCode.RpcFrameworkError, 'Received array has not exact 4 items')
+    }
+    if (data[0] !== OcppMessageTypeIdEnum.Call) {
+      throw new OcppWsException(OcppErrorCode.RpcFrameworkError, 'MessageType is not 2', data[1])
+    }
+    if (!Object.values(OcppMessageEnum).includes(data[2])) {
+      throw new OcppWsException(OcppErrorCode.NotImplemented, data[2], data[1])
+    }
 
+    const obj = {
+      messageTypeId: data[0],
+      messageId: data[1],
+      action: data[2],
+      payload: data[3],
+    }
+
+    const ocppCall = plainToClass(OcppCallDto, obj)
+    await this.validate(ocppCall, ocppCall.messageId)
+    return ocppCall
+  }
+
+  @SubscribeMessage('ocpp')
+  async ocppCommand(
+    @MessageBody() data: unknown,
+    @ConnectedSocket() client: Socket,
+  ): Promise<[number, string, IResponseMessage] | any> {
+    const ocppCall = await this.convertInputToOcppCall(data)
+    const chargingStation = this.chargingStations.find((x) => x.socketId === client.id)
+    // ToDo: Was ist, wenn es keine ChargingStation gibt?
+    console.log(chargingStation)
+
+    let response: IResponseMessage
     switch (ocppCall.action) {
       case OcppMessageEnum.BootNotification:
         const entityClass = plainToClass(BootNotificationRequestDto, ocppCall.payload as unknown)
