483e3ba8b0cb3164ebf0c2dcf8b72a085c8fb330
Steffen <steffen@4s1.de>
2021-01-07 18:17:23 +0100

    NestJS Code entfernt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -67,3 +67,65 @@ ws.on('connection', (ws: any, request: any, client: any) => {
 })
 
 server.listen(3000)
+
+// ---------------------------------
+
+// private async convertInputToOcppCall(data: unknown): Promise<OcppCallDto> {
+//   if (!data) {
+//     throw new OcppWsException(OcppErrorCode.RpcFrameworkError, 'Invalid data format received')
+//   }
+//   if (!Array.isArray(data)) {
+//     throw new OcppWsException(OcppErrorCode.RpcFrameworkError, 'No array received')
+//   }
+//   if (data.length !== 4) {
+//     throw new OcppWsException(OcppErrorCode.RpcFrameworkError, 'Received array has not exact 4 items')
+//   }
+//   if (data[0] !== OcppMessageTypeIdEnum.Call) {
+//     throw new OcppWsException(OcppErrorCode.RpcFrameworkError, 'MessageType is not 2', data[1])
+//   }
+//   if (!Object.values(OcppMessageEnum).includes(data[2])) {
+//     throw new OcppWsException(OcppErrorCode.NotImplemented, data[2], data[1])
+//   }
+
+//   const obj = {
+//     messageTypeId: data[0],
+//     messageId: data[1],
+//     action: data[2],
+//     payload: data[3],
+//   }
+
+//   const ocppCall = plainToClass(OcppCallDto, obj)
+//   await this.validate(ocppCall, ocppCall.messageId)
+//   return ocppCall
+// }
+
+// ---------------------------------
+
+// public async handleConnection(client: Socket): Promise<void> {
+//   this.logger.log(`Client connected: ${client.id}`)
+//   const req: IncomingMessage = client.request as IncomingMessage
+//   const parts = req.url?.split('/')
+//   if (parts && parts.length >= 4) {
+//     const chargingStation = this.getChargingStation(parts[3])
+//     chargingStation.socketId = client.id
+//   } else {
+//     this.logger.log(`Client URL is invalid "${req.url}"`)
+//     client.disconnect()
+//   }
+// }
+
+// ---------------------------------
+
+// private chargingStations: ChargingStation[] = []
+
+// private getChargingStation(uniqueIdentifier: string): ChargingStation {
+//   let chargingStation = this.chargingStations.find((x) => x.uniqueIdentifier === uniqueIdentifier)
+//   if (!chargingStation) {
+//     chargingStation = new ChargingStation(uniqueIdentifier)
+//     this.chargingStations.push(chargingStation)
+//   }
+//   return chargingStation
+// }
+
+// ---------------------------------
+// ---------------------------------
