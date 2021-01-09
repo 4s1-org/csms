6f49d10e01e84e1f9393b1001df200ad77288b2b
Steffen <yellowgarbagebag@4s1.de>
2021-01-09 17:58:32 +0100

    pino ohne Ausgaben in Tests

--- backend/src/logger.ts
+++ backend/src/logger.ts
@@ -2,6 +2,7 @@ import pino from 'pino'
 
 export function createLogger(name: string): pino.Logger {
   const logger = pino({
+    enabled: !!process.env.NOLOG,
     prettyPrint: { colorize: true },
     name,
     base: {
