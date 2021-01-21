cc9ecbc24f6c9c39f95f5ac97b6a7da923399829
Steffen <yellowgarbagebag@4s1.de>
2021-01-21 16:48:46 +0100

    Kommentare für TransactionEvent

--- shared/src/messages/transaction-event-response.dto.ts
+++ shared/src/messages/transaction-event-response.dto.ts
@@ -6,6 +6,9 @@ import { CustomDataDto } from '../datatypes/custom-data.dto'
 import { IdTokenInfoDto } from '../datatypes/id-token-info.dto'
 import { MessageContentDto } from '../datatypes/message-content.dto'
 
+/**
+ * This contains the field definition of the TransactionEventResponse PDU sent by the CSMS to the Charging Station in response to a TransactionEventRequest.
+ */
 export class TransactionEventResponseDto extends ResponseBaseDto {
   public constructor() {
     super()
@@ -15,18 +18,36 @@ export class TransactionEventResponseDto extends ResponseBaseDto {
   public customData!: CustomDataDto
 
   /**
-   * SHALL only be sent when charging has ended. Final total cost of this transaction, including taxes. In the currency configured with the Configuration Variable: <<configkey-currency,`Currency`>>. When omitted, the transaction was NOT free. To indicate a free transaction, the CSMS SHALL send 0.00.
+   * SHALL only be sent when charging has ended. Final total cost of this transaction, including taxes. In the currency configured with the Configuration Variable: Currency. When omitted, the transaction was NOT free. To indicate a free transaction, the CSMS SHALL send 0.00.
+   * Required: false
+   * decimal
+   * 0..1
    */
   public totalCost!: number
 
   /**
-   * Priority from a business point of view. Default priority is 0, The range is from -9 to 9. Higher values indicate a higher priority. The chargingPriority in <<transactioneventresponse,TransactionEventResponse>> is temporarily, so it may not be set in the <<cmn_idtokeninfotype,IdTokenInfoType>> afterwards. Also the chargingPriority in <<transactioneventresponse,TransactionEventResponse>> overrules the one in <<cmn_idtokeninfotype,IdTokenInfoType>>.
+   * Priority from a business point of view. Default priority is 0, The range is from -9 to 9. Higher values indicate a higher priority. The chargingPriority in TransactionEventResponse is temporarily, so it may not be set in the IdTokenInfoType afterwards. Also the chargingPriority in TransactionEventResponse overrules the one in IdTokenInfoType.
+   * Required: false
+   * integer
+   * 0..1
    */
   public chargingPriority!: number
 
+  /**
+   * This contains information about authorization status, expiry and group id. Is required when the transactionEventRequest contained an idToken.
+   * Required: false
+   * IdTokenInfoType
+   * 0..1
+   */
   @Type(() => IdTokenInfoDto)
   public idTokenInfo!: IdTokenInfoDto
 
+  /**
+   * This can contain updated personal message that can be shown to the EV Driver. This can be used to provide updated tariff information.
+   * Required: false
+   * MessageContentType
+   * 0..1
+   */
   @Type(() => MessageContentDto)
   public updatedPersonalMessage!: MessageContentDto
 }
