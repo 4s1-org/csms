96a38fb98b8345ec29b9cce95702d97c586a2483
Steffen <steffen@4s1.de>
2021-01-21 16:16:25 +0100

    Heartbeat

--- shared/src/utils/logger.ts
+++ shared/src/utils/logger.ts
@@ -5,10 +5,7 @@ export type LogLevel = 'fatal' | 'error' | 'warn' | 'info' | 'debug' | 'trace' |
 export class Logger {
   private _logger: pino.Logger
 
-  public constructor(
-    public readonly name: string,
-    logLevel: LogLevel = "debug"
-  ) {
+  public constructor(public readonly name: string, logLevel: LogLevel = 'debug') {
     this._logger = pino({
       enabled: process.env.NODE_ENV !== 'test',
       level: logLevel,
@@ -20,52 +17,59 @@ export class Logger {
     })
   }
 
-  public fatal(msg: string, obj?: any): void {
-    this._logger.fatal(msg)
+  public fatal(msg: string, obj?: unknown): void {
     if (obj !== undefined) {
-      this._logger.fatal(obj, msg)
+      this._logger.fatal(`${msg} | ${obj}`)
+    } else {
+      this._logger.fatal(msg)
     }
   }
 
-  public error(msg: string, obj?: any): void {
-    this._logger.error(msg)
+  public error(msg: string, obj?: unknown): void {
     if (obj !== undefined) {
-      this._logger.error(obj, msg)
+      this._logger.error(`${msg} | ${obj}`)
+    } else {
+      this._logger.error(msg)
     }
   }
 
-  public warn(msg: string, obj?: any): void {
-    this._logger.warn(msg)
+  public warn(msg: string, obj?: unknown): void {
     if (obj !== undefined) {
-      this._logger.warn(obj, msg)
+      this._logger.warn(`${msg} | ${obj}`)
+    } else {
+      this._logger.warn(msg)
     }
   }
 
-  public info(msg: string, obj?: any): void {
-    this._logger.info(msg)
+  public info(msg: string, obj?: unknown): void {
     if (obj !== undefined) {
-      this._logger.info(obj, msg)
+      this._logger.info(`${msg} | ${obj}`)
+    } else {
+      this._logger.info(msg)
     }
   }
 
-  public debug(msg: string, obj?: any): void {
-    this._logger.debug(msg)
+  public debug(msg: string, obj?: unknown): void {
     if (obj !== undefined) {
-      this._logger.debug(obj)
+      this._logger.debug(`${msg} | ${obj}`)
+    } else {
+      this._logger.debug(msg)
     }
   }
 
-  public trace(msg: string, obj?: any): void {
-    this._logger.trace(msg)
+  public trace(msg: string, obj?: unknown): void {
     if (obj !== undefined) {
-      this._logger.trace(obj)
+      this._logger.trace(`${msg} | ${obj}`)
+    } else {
+      this._logger.trace(msg)
     }
   }
 
-  public silent(msg: string, obj?: any): void {
-    this._logger.silent(msg)
+  public silent(msg: string, obj?: unknown): void {
     if (obj !== undefined) {
-      this._logger.silent(obj, msg)
+      this._logger.silent(`${msg} | ${obj}`)
+    } else {
+      this._logger.silent(msg)
     }
   }
 }
