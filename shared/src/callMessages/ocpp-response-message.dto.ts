c76680134d12bb60df9150002f40a1f10c61cfa1
Steffen <steffen@4s1.de>
2021-01-21 13:41:58 +0100

    Validierung überarbeitet und Klassennamen korrigiert

---
+++ shared/src/callMessages/ocpp-response-message.dto.ts
@@ -0,0 +1,17 @@
+import { ResponseBaseDto } from '../generated/response-base.dto'
+import { OcppBaseMessageDto } from './ocpp-base-message.dto'
+import { OcppMessageTypeIdEnum } from './ocpp-message-type-id.enum'
+
+export class OcppResponseMessageDto extends OcppBaseMessageDto {
+  constructor(messageId: string, payload: ResponseBaseDto) {
+    super(OcppMessageTypeIdEnum.Result, messageId)
+    this.payload = payload
+  }
+
+  /** JSON Payload of the action. */
+  public payload: ResponseBaseDto
+
+  public toString(): string {
+    return JSON.stringify([this.messageTypeId, this.messageId, this.payload])
+  }
+}
