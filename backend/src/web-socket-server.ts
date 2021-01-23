f1657349ea1487bfb3f416fc45f7c027f97e8a33
Steffen <yellowgarbagebag@4s1.de>
2021-01-23 18:16:23 +0100

    Erster Wurf zur authenticate

--- backend/src/web-socket-server.ts
+++ backend/src/web-socket-server.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import https from 'https'
 import { WebSocketServerBase } from './web-socket-server-base'
 import { IncomingMessage } from 'http'
+import { TLSSocket } from 'tls'
 
 export class WebSocketServer extends WebSocketServerBase {
   private server: https.Server | undefined
@@ -13,22 +14,28 @@ export class WebSocketServer extends WebSocketServerBase {
   }
 
   public start(): void {
-    this.server = https.createServer(
-      {
-        cert: fs.readFileSync(path.join(__dirname, '..', 'third-party', 'certificates', 'localhost-chain.pem')),
-        key: fs.readFileSync(path.join(__dirname, '..', 'third-party', 'certificates', 'localhost.key')),
-      },
-      (req, res) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
-        res.write('Server is running!')
-        res.end()
-      },
-    )
-
+    this.server = https.createServer({
+      cert: fs.readFileSync(path.join(__dirname, '..', 'third-party', 'certificates', 'localhost-chain.pem')),
+      key: fs.readFileSync(path.join(__dirname, '..', 'third-party', 'certificates', 'localhost.key')),
+    })
     const wss = new WebSocket.Server({
       server: this.server,
+      //path: '/ocpp',
     })
-    wss.on('connection', (socket: WebSocket, request: IncomingMessage) => this.onConnection(socket, request))
+
+    wss.on('connection', (socket: WebSocket, request: IncomingMessage) => {
+      this.onConnection(socket, request)
+    })
+
+    this.server.on('upgrade', (request: IncomingMessage, socket: TLSSocket): void => {
+      const authenticated: boolean = this.authenticate(request)
+
+      if (!authenticated) {
+        socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n')
+        socket.destroy()
+        return
+      }
+    })
 
     this.server.listen(this.port)
     this.logger.info(`WebSocketServer is running on port ${this.port}`)
@@ -39,5 +46,9 @@ export class WebSocketServer extends WebSocketServerBase {
       this.server.close()
     }
     this.logger.info(`WebSocketServer stopped`)
+  }
+
+  private authenticate(request: IncomingMessage): boolean {
+    return true
   }
 }
