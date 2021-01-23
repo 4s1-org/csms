7d0d41e8ce2df9ee488f1c9386bcd9a04247f5f3
Steffen <yellowgarbagebag@4s1.de>
2021-01-23 15:27:38 +0100

    Errorhandler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,15 @@
 import { WebSocketServer } from './web-socket-server'
 
+process.on('uncaughtException', (err) => {
+  console.log(`Uncaught Exception: ${err.message}`)
+  process.exit(1)
+})
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.log('Unhandled rejection at ', promise, `reason: ${reason}`)
+  process.exit(1)
+})
+
 const server = new WebSocketServer('127.0.0.1', 3000)
 try {
   server.start()
