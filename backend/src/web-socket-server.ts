ba3eb71172122dc2b5ffbf2768891e5f7f8acdfb
Steffen <yellowgarbagebag@4s1.de>
2021-01-28 15:13:54 +0100

    Server vereinfacht

--- backend/src/web-socket-server.ts
+++ backend/src/web-socket-server.ts
@@ -5,20 +5,7 @@ import https from 'https'
 import { IncomingMessage } from 'http'
 import { TLSSocket } from 'tls'
 import { ChargingStation } from './charging-station'
-import {
-  Logger,
-  OcppErrorCodeEnum,
-  ResponseBaseDto,
-  CsmsError,
-  OcppBaseCallDto,
-  CallConverter,
-  OcppRequestCallDto,
-  PayloadConverter,
-  OcppResponseCallDto,
-  OcppErrorCallDto,
-  CsmsCallValidationError,
-  PayloadValidator,
-} from '@yellowgarbagebag/csms-shared'
+import { Logger, OcppBaseCallDto, handleIncomingCall } from '@yellowgarbagebag/csms-shared'
 import { DataProvider } from './data-provider'
 
 export class WebSocketServer {
@@ -44,9 +31,9 @@ export class WebSocketServer {
       }
 
       socket.onmessage = (msg: WebSocket.MessageEvent): void => {
-        const result: string | undefined = this.handleIncomingCall(cs, msg.data)
+        const result: OcppBaseCallDto | undefined = handleIncomingCall(cs, msg.data)
         if (result) {
-          socket.send(result)
+          socket.send(result.toCallString())
         }
       }
     })
@@ -102,74 +89,6 @@ export class WebSocketServer {
       return cs
     } else {
       return undefined
-    }
-  }
-
-  private handleIncomingCall(cs: ChargingStation, data: any): string | undefined {
-    // Brauche im Fehlerfall
-    let call: OcppBaseCallDto | undefined
-
-    try {
-      cs.logger.debug('Received', data)
-      // Das "Array" validieren
-      call = CallConverter.instance.convert(data)
-
-      if (call instanceof OcppRequestCallDto) {
-        cs.logger.info(`Incoming Request | ${call.action} | ${call.messageId}`)
-        PayloadValidator.instance.validateRequest(call)
-        PayloadConverter.instance.convertRequest(call)
-        // Verarbeitung der Daten
-        const responsePayload: ResponseBaseDto = cs.incomingRequestCall(call.payload)
-        // Antwortobjekt erstellen
-        const responseCall = new OcppResponseCallDto(call.messageId, responsePayload)
-        // Anwortdaten validieren (nice to have)
-        try {
-          PayloadValidator.instance.validateResponse(responseCall, call.action)
-        } catch (err) {
-          if (err instanceof CsmsError) {
-            cs.logger.error(`Server send invalid data | ${err.errorCode} | ${err.errorDescription}`)
-          } else {
-            throw err
-          }
-        }
-        // Loggen und senden
-        cs.logger.info(`Outgoing Response | ${call.action} | ${call.messageId}`)
-        cs.logger.debug('Send', responseCall)
-        return responseCall.toString()
-      } else if (call instanceof OcppResponseCallDto) {
-        const action = cs.getActionToRequest(call.messageId)
-        cs.logger.info(`Incoming Response | ${action} | ${call.messageId}`)
-        PayloadValidator.instance.validateResponse(call, action)
-        PayloadConverter.instance.convertResponse(call, action)
-        cs.incomingResponseCall(call.payload)
-        // Auf eine Antwort wird keine Antwort gesendet
-        return undefined
-      } else if (call instanceof OcppErrorCallDto) {
-        const action = cs.getActionToRequest(call.messageId)
-        cs.logger.info(`Incoming Error | ${action} | ${call.messageId}`)
-        cs.incomingErrorCall(call)
-        // Auf einen Fehler wird keine Antwort gesendet
-        return undefined
-      } else {
-        // Das kann eigentlich nie passieren
-        throw new CsmsError(OcppErrorCodeEnum.InternalError, 'Unknown call')
-      }
-    } catch (err) {
-      const logger: Logger = cs?.logger || this.logger
-
-      if (err instanceof CsmsCallValidationError) {
-        logger.warn(`Validation Error | ${err.errorCode} | ${err.errorDescription}`)
-        return new OcppErrorCallDto(err.messageId, err.errorCode, err.errorDescription).toString()
-      } else if (err instanceof CsmsError) {
-        const messageId: string = call?.messageId || ''
-        logger.warn(`Csms Error | ${err.errorCode} | ${err.errorDescription}`)
-        return new OcppErrorCallDto(messageId, err.errorCode, err.errorDescription).toString()
-      } else {
-        const messageId: string = call?.messageId || ''
-        logger.fatal('Internal Server Error')
-        logger.fatal(err)
-        return new OcppErrorCallDto(messageId, OcppErrorCodeEnum.InternalError).toString()
-      }
     }
   }
 }
