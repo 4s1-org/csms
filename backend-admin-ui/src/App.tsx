6bcef041725acdb7bbea49a880945e0f97915557
Steffen <yellowgarbagebag@4s1.de>
2021-02-07 14:29:58 +0100

    Format code

--- backend-admin-ui/src/App.tsx
+++ backend-admin-ui/src/App.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import "./App.css";
+import React from 'react'
+import './App.css'
 
 function App() {
-  var authToken = "YWRtaW46YWRtaW4="; // admin:admin
-  document.cookie = "X-Authorization=" + authToken + "; path=/";
+  var authToken = 'YWRtaW46YWRtaW4=' // admin:admin
+  document.cookie = 'X-Authorization=' + authToken + '; path=/'
 
-  const ws = new WebSocket("wss://localhost:3000/admin", ["ocpp2.0.1"]);
+  const ws = new WebSocket('wss://localhost:3000/admin', ['ocpp2.0.1'])
 
   ws.onopen = () => {
-    console.log("IT WORKS");
-  };
-  ws.onmessage = (msg: any): void => {};
-  ws.onerror = (msg: any): void => {};
-  ws.onclose = (): void => {};
+    console.log('IT WORKS')
+  }
+  ws.onmessage = (msg: any): void => {}
+  ws.onerror = (msg: any): void => {}
+  ws.onclose = (): void => {}
 
   return (
     <div className="App">
@@ -20,17 +20,12 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- charging-station-ui/src/App.tsx
+++ charging-station-ui/src/App.tsx
@@ -1,38 +1,35 @@
-import React from "react";
-import "./App.css";
-import {
-  WebSocketClient,
-  ChargingStation,
-} from "@yellowgarbagebag/csms-charging-station-lib";
+import React from 'react'
+import './App.css'
+import { WebSocketClient, ChargingStation } from '@yellowgarbagebag/csms-charging-station-lib'
 
 function App() {
-  var authToken = "TFMwMDE6dGVzdA="; // LS001:test
-  document.cookie = "X-Authorization=" + authToken + "; path=/";
+  var authToken = 'TFMwMDE6dGVzdA=' // LS001:test
+  document.cookie = 'X-Authorization=' + authToken + '; path=/'
 
-  const cs = new ChargingStation("LS001", "LS001", "test");
-  const ws = new WebSocket("wss://localhost:3000/ocpp/LS001", ["ocpp2.0.1"]);
+  const cs = new ChargingStation('LS001', 'LS001', 'test')
+  const ws = new WebSocket('wss://localhost:3000/ocpp/LS001', ['ocpp2.0.1'])
 
   const sendCallback = (msg: any): boolean => {
     if (ws && ws.OPEN) {
-      ws.send(msg);
-      return true;
+      ws.send(msg)
+      return true
     }
-    return false;
-  };
+    return false
+  }
 
-  const client = new WebSocketClient(cs, sendCallback);
+  const client = new WebSocketClient(cs, sendCallback)
   ws.onopen = () => {
-    client.send(cs.sendBootNotificationRequest());
-  };
+    client.send(cs.sendBootNotificationRequest())
+  }
   ws.onmessage = (msg: any): void => {
     //client.onMessage(msg.data);
-  };
+  }
   ws.onerror = (msg: any): void => {
-    client.onError(msg);
-  };
+    client.onError(msg)
+  }
   ws.onclose = (): void => {
-    client.onClose();
-  };
+    client.onClose()
+  }
 
   return (
     <div className="App">
@@ -40,17 +37,12 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
