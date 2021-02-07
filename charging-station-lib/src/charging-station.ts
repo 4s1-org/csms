16e8386cdb4bced7ce6c88b2562f1ec3cabff357
Steffen <steffen@4s1.de>
2021-02-07 14:16:49 +0100

    Projekte erweitert

--- charging-station-lib/src/charging-station.ts
+++ charging-station-lib/src/charging-station.ts
@@ -1,7 +1,6 @@
 import {
   BootNotificationRequestDto,
   BootNotificationResponseDto,
-  Logger,
   OcppErrorCodeEnum,
   RequestBaseDto,
   ResponseBaseDto,
@@ -44,7 +43,8 @@ import {
   EventNotificationEnum,
   VariableDto,
   NotifyEventResponseDto,
-} from '@yellowgarbagebag/csms-shared'
+} from '@yellowgarbagebag/csms-ocpp-lib'
+import { Logger } from '@yellowgarbagebag/csms-common-lib'
 
 export class ChargingStation implements IChargingStation {
   public readonly logger = new Logger(this.uniqueIdentifier)
--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ pnpm install
 pnpm run build --prefix common-lib
 pnpm run build --prefix ocpp-lib
 pnpm run build --prefix backend
-#pnpm run build --prefix backend-admin-ui
+pnpm run build --prefix backend-admin-ui
 pnpm run build --prefix charging-station-lib
 pnpm run build --prefix charging-station-cli
 pnpm run build --prefix charging-station-ui
