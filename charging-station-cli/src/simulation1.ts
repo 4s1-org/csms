5c046210771458d5a7d70abd3bcb3ba485d9a030
Steffen <yellowgarbagebag@4s1.de>
2021-02-07 16:49:59 +0100

    CLI Simulator geht

---
+++ charging-station-cli/src/simulation1.ts
@@ -0,0 +1,65 @@
+import { ChargingStation, WebSocketClient } from '@yellowgarbagebag/csms-charging-station-lib'
+import WebSocket from 'ws'
+
+const cs = new ChargingStation('LS001', 'LS001', 'test')
+const socket = new WebSocket(`wss://localhost:3000/ocpp/${cs.uniqueIdentifier}`, ['ocpp2.0.1'], {
+  headers: {
+    authorization: `Basic ${Buffer.from(`${cs.username}:${cs.password}`).toString('base64')}`,
+  },
+})
+
+const sendCallback = (msg: any): boolean => {
+  if (socket && socket.OPEN) {
+    socket.send(msg)
+    return true
+  }
+  return false
+}
+const client = new WebSocketClient(cs, sendCallback)
+
+socket.onopen = (): void => {
+  cs.connect()
+
+  client.send(cs.sendBootNotificationRequest())
+
+  setTimeout(() => {
+    client.send(cs.sendStatusNotificationRequest())
+  }, 100)
+
+  setTimeout(() => {
+    client.send(cs.sendAuthorizationRequest_PinCode())
+  }, 200)
+
+  setTimeout(() => {
+    client.send(cs.sendAuthorizationRequest_Rfid())
+  }, 300)
+
+  setTimeout(() => {
+    client.send(cs.sendMeterValueRequest())
+  }, 400)
+
+  setTimeout(() => {
+    client.send(cs.sendHeartbeatRequest())
+  }, 500)
+
+  setTimeout(() => {
+    client.send(cs.sendNotifyEventRequest_LockFailure())
+  }, 600)
+
+  setTimeout(() => {
+    socket.close()
+  }, 1000)
+}
+
+// Handling, besonders der Fehler, wie im Backend lösen
+socket.onmessage = (msg: WebSocket.MessageEvent): void => {
+  client.onMessage(msg.data)
+}
+
+socket.onerror = (err: WebSocket.ErrorEvent): void => {
+  cs.logger.error(err.message)
+}
+
+socket.onclose = (): void => {
+  cs.logger.info('Connection closed')
+}
