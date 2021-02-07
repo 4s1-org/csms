0ec943bc2f2186fb24e62fe0c8c451302e9dec18
Steffen <yellowgarbagebag@4s1.de>
2021-02-07 17:35:55 +0100

    Laden der JSON Schema überarbeitet

---
+++ ocpp-lib/src/generated/json-schema-imports.ts
@@ -0,0 +1,261 @@
+// THIS FILE IS AUTO-GENERATED. DO NOT CHANGE IT!
+
+import AuthorizeRequest from './../third-party/ocpp/2.0.1/AuthorizeRequest.json'
+import AuthorizeResponse from './../third-party/ocpp/2.0.1/AuthorizeResponse.json'
+import BootNotificationRequest from './../third-party/ocpp/2.0.1/BootNotificationRequest.json'
+import BootNotificationResponse from './../third-party/ocpp/2.0.1/BootNotificationResponse.json'
+import CancelReservationRequest from './../third-party/ocpp/2.0.1/CancelReservationRequest.json'
+import CancelReservationResponse from './../third-party/ocpp/2.0.1/CancelReservationResponse.json'
+import CertificateSignedRequest from './../third-party/ocpp/2.0.1/CertificateSignedRequest.json'
+import CertificateSignedResponse from './../third-party/ocpp/2.0.1/CertificateSignedResponse.json'
+import ChangeAvailabilityRequest from './../third-party/ocpp/2.0.1/ChangeAvailabilityRequest.json'
+import ChangeAvailabilityResponse from './../third-party/ocpp/2.0.1/ChangeAvailabilityResponse.json'
+import ClearCacheRequest from './../third-party/ocpp/2.0.1/ClearCacheRequest.json'
+import ClearCacheResponse from './../third-party/ocpp/2.0.1/ClearCacheResponse.json'
+import ClearChargingProfileRequest from './../third-party/ocpp/2.0.1/ClearChargingProfileRequest.json'
+import ClearChargingProfileResponse from './../third-party/ocpp/2.0.1/ClearChargingProfileResponse.json'
+import ClearDisplayMessageRequest from './../third-party/ocpp/2.0.1/ClearDisplayMessageRequest.json'
+import ClearDisplayMessageResponse from './../third-party/ocpp/2.0.1/ClearDisplayMessageResponse.json'
+import ClearVariableMonitoringRequest from './../third-party/ocpp/2.0.1/ClearVariableMonitoringRequest.json'
+import ClearVariableMonitoringResponse from './../third-party/ocpp/2.0.1/ClearVariableMonitoringResponse.json'
+import ClearedChargingLimitRequest from './../third-party/ocpp/2.0.1/ClearedChargingLimitRequest.json'
+import ClearedChargingLimitResponse from './../third-party/ocpp/2.0.1/ClearedChargingLimitResponse.json'
+import CostUpdatedRequest from './../third-party/ocpp/2.0.1/CostUpdatedRequest.json'
+import CostUpdatedResponse from './../third-party/ocpp/2.0.1/CostUpdatedResponse.json'
+import CustomerInformationRequest from './../third-party/ocpp/2.0.1/CustomerInformationRequest.json'
+import CustomerInformationResponse from './../third-party/ocpp/2.0.1/CustomerInformationResponse.json'
+import DataTransferRequest from './../third-party/ocpp/2.0.1/DataTransferRequest.json'
+import DataTransferResponse from './../third-party/ocpp/2.0.1/DataTransferResponse.json'
+import DeleteCertificateRequest from './../third-party/ocpp/2.0.1/DeleteCertificateRequest.json'
+import DeleteCertificateResponse from './../third-party/ocpp/2.0.1/DeleteCertificateResponse.json'
+import FirmwareStatusNotificationRequest from './../third-party/ocpp/2.0.1/FirmwareStatusNotificationRequest.json'
+import FirmwareStatusNotificationResponse from './../third-party/ocpp/2.0.1/FirmwareStatusNotificationResponse.json'
+import Get15118EVCertificateRequest from './../third-party/ocpp/2.0.1/Get15118EVCertificateRequest.json'
+import Get15118EVCertificateResponse from './../third-party/ocpp/2.0.1/Get15118EVCertificateResponse.json'
+import GetBaseReportRequest from './../third-party/ocpp/2.0.1/GetBaseReportRequest.json'
+import GetBaseReportResponse from './../third-party/ocpp/2.0.1/GetBaseReportResponse.json'
+import GetCertificateStatusRequest from './../third-party/ocpp/2.0.1/GetCertificateStatusRequest.json'
+import GetCertificateStatusResponse from './../third-party/ocpp/2.0.1/GetCertificateStatusResponse.json'
+import GetChargingProfilesRequest from './../third-party/ocpp/2.0.1/GetChargingProfilesRequest.json'
+import GetChargingProfilesResponse from './../third-party/ocpp/2.0.1/GetChargingProfilesResponse.json'
+import GetCompositeScheduleRequest from './../third-party/ocpp/2.0.1/GetCompositeScheduleRequest.json'
+import GetCompositeScheduleResponse from './../third-party/ocpp/2.0.1/GetCompositeScheduleResponse.json'
+import GetDisplayMessagesRequest from './../third-party/ocpp/2.0.1/GetDisplayMessagesRequest.json'
+import GetDisplayMessagesResponse from './../third-party/ocpp/2.0.1/GetDisplayMessagesResponse.json'
+import GetInstalledCertificateIdsRequest from './../third-party/ocpp/2.0.1/GetInstalledCertificateIdsRequest.json'
+import GetInstalledCertificateIdsResponse from './../third-party/ocpp/2.0.1/GetInstalledCertificateIdsResponse.json'
+import GetLocalListVersionRequest from './../third-party/ocpp/2.0.1/GetLocalListVersionRequest.json'
+import GetLocalListVersionResponse from './../third-party/ocpp/2.0.1/GetLocalListVersionResponse.json'
+import GetLogRequest from './../third-party/ocpp/2.0.1/GetLogRequest.json'
+import GetLogResponse from './../third-party/ocpp/2.0.1/GetLogResponse.json'
+import GetMonitoringReportRequest from './../third-party/ocpp/2.0.1/GetMonitoringReportRequest.json'
+import GetMonitoringReportResponse from './../third-party/ocpp/2.0.1/GetMonitoringReportResponse.json'
+import GetReportRequest from './../third-party/ocpp/2.0.1/GetReportRequest.json'
+import GetReportResponse from './../third-party/ocpp/2.0.1/GetReportResponse.json'
+import GetTransactionStatusRequest from './../third-party/ocpp/2.0.1/GetTransactionStatusRequest.json'
+import GetTransactionStatusResponse from './../third-party/ocpp/2.0.1/GetTransactionStatusResponse.json'
+import GetVariablesRequest from './../third-party/ocpp/2.0.1/GetVariablesRequest.json'
+import GetVariablesResponse from './../third-party/ocpp/2.0.1/GetVariablesResponse.json'
+import HeartbeatRequest from './../third-party/ocpp/2.0.1/HeartbeatRequest.json'
+import HeartbeatResponse from './../third-party/ocpp/2.0.1/HeartbeatResponse.json'
+import InstallCertificateRequest from './../third-party/ocpp/2.0.1/InstallCertificateRequest.json'
+import InstallCertificateResponse from './../third-party/ocpp/2.0.1/InstallCertificateResponse.json'
+import LogStatusNotificationRequest from './../third-party/ocpp/2.0.1/LogStatusNotificationRequest.json'
+import LogStatusNotificationResponse from './../third-party/ocpp/2.0.1/LogStatusNotificationResponse.json'
+import MeterValuesRequest from './../third-party/ocpp/2.0.1/MeterValuesRequest.json'
+import MeterValuesResponse from './../third-party/ocpp/2.0.1/MeterValuesResponse.json'
+import NotifyChargingLimitRequest from './../third-party/ocpp/2.0.1/NotifyChargingLimitRequest.json'
+import NotifyChargingLimitResponse from './../third-party/ocpp/2.0.1/NotifyChargingLimitResponse.json'
+import NotifyCustomerInformationRequest from './../third-party/ocpp/2.0.1/NotifyCustomerInformationRequest.json'
+import NotifyCustomerInformationResponse from './../third-party/ocpp/2.0.1/NotifyCustomerInformationResponse.json'
+import NotifyDisplayMessagesRequest from './../third-party/ocpp/2.0.1/NotifyDisplayMessagesRequest.json'
+import NotifyDisplayMessagesResponse from './../third-party/ocpp/2.0.1/NotifyDisplayMessagesResponse.json'
+import NotifyEVChargingNeedsRequest from './../third-party/ocpp/2.0.1/NotifyEVChargingNeedsRequest.json'
+import NotifyEVChargingNeedsResponse from './../third-party/ocpp/2.0.1/NotifyEVChargingNeedsResponse.json'
+import NotifyEVChargingScheduleRequest from './../third-party/ocpp/2.0.1/NotifyEVChargingScheduleRequest.json'
+import NotifyEVChargingScheduleResponse from './../third-party/ocpp/2.0.1/NotifyEVChargingScheduleResponse.json'
+import NotifyEventRequest from './../third-party/ocpp/2.0.1/NotifyEventRequest.json'
+import NotifyEventResponse from './../third-party/ocpp/2.0.1/NotifyEventResponse.json'
+import NotifyMonitoringReportRequest from './../third-party/ocpp/2.0.1/NotifyMonitoringReportRequest.json'
+import NotifyMonitoringReportResponse from './../third-party/ocpp/2.0.1/NotifyMonitoringReportResponse.json'
+import NotifyReportRequest from './../third-party/ocpp/2.0.1/NotifyReportRequest.json'
+import NotifyReportResponse from './../third-party/ocpp/2.0.1/NotifyReportResponse.json'
+import PublishFirmwareRequest from './../third-party/ocpp/2.0.1/PublishFirmwareRequest.json'
+import PublishFirmwareResponse from './../third-party/ocpp/2.0.1/PublishFirmwareResponse.json'
+import PublishFirmwareStatusNotificationRequest from './../third-party/ocpp/2.0.1/PublishFirmwareStatusNotificationRequest.json'
+import PublishFirmwareStatusNotificationResponse from './../third-party/ocpp/2.0.1/PublishFirmwareStatusNotificationResponse.json'
+import ReportChargingProfilesRequest from './../third-party/ocpp/2.0.1/ReportChargingProfilesRequest.json'
+import ReportChargingProfilesResponse from './../third-party/ocpp/2.0.1/ReportChargingProfilesResponse.json'
+import RequestStartTransactionRequest from './../third-party/ocpp/2.0.1/RequestStartTransactionRequest.json'
+import RequestStartTransactionResponse from './../third-party/ocpp/2.0.1/RequestStartTransactionResponse.json'
+import RequestStopTransactionRequest from './../third-party/ocpp/2.0.1/RequestStopTransactionRequest.json'
+import RequestStopTransactionResponse from './../third-party/ocpp/2.0.1/RequestStopTransactionResponse.json'
+import ReservationStatusUpdateRequest from './../third-party/ocpp/2.0.1/ReservationStatusUpdateRequest.json'
+import ReservationStatusUpdateResponse from './../third-party/ocpp/2.0.1/ReservationStatusUpdateResponse.json'
+import ReserveNowRequest from './../third-party/ocpp/2.0.1/ReserveNowRequest.json'
+import ReserveNowResponse from './../third-party/ocpp/2.0.1/ReserveNowResponse.json'
+import ResetRequest from './../third-party/ocpp/2.0.1/ResetRequest.json'
+import ResetResponse from './../third-party/ocpp/2.0.1/ResetResponse.json'
+import SecurityEventNotificationRequest from './../third-party/ocpp/2.0.1/SecurityEventNotificationRequest.json'
+import SecurityEventNotificationResponse from './../third-party/ocpp/2.0.1/SecurityEventNotificationResponse.json'
+import SendLocalListRequest from './../third-party/ocpp/2.0.1/SendLocalListRequest.json'
+import SendLocalListResponse from './../third-party/ocpp/2.0.1/SendLocalListResponse.json'
+import SetChargingProfileRequest from './../third-party/ocpp/2.0.1/SetChargingProfileRequest.json'
+import SetChargingProfileResponse from './../third-party/ocpp/2.0.1/SetChargingProfileResponse.json'
+import SetDisplayMessageRequest from './../third-party/ocpp/2.0.1/SetDisplayMessageRequest.json'
+import SetDisplayMessageResponse from './../third-party/ocpp/2.0.1/SetDisplayMessageResponse.json'
+import SetMonitoringBaseRequest from './../third-party/ocpp/2.0.1/SetMonitoringBaseRequest.json'
+import SetMonitoringBaseResponse from './../third-party/ocpp/2.0.1/SetMonitoringBaseResponse.json'
+import SetMonitoringLevelRequest from './../third-party/ocpp/2.0.1/SetMonitoringLevelRequest.json'
+import SetMonitoringLevelResponse from './../third-party/ocpp/2.0.1/SetMonitoringLevelResponse.json'
+import SetNetworkProfileRequest from './../third-party/ocpp/2.0.1/SetNetworkProfileRequest.json'
+import SetNetworkProfileResponse from './../third-party/ocpp/2.0.1/SetNetworkProfileResponse.json'
+import SetVariableMonitoringRequest from './../third-party/ocpp/2.0.1/SetVariableMonitoringRequest.json'
+import SetVariableMonitoringResponse from './../third-party/ocpp/2.0.1/SetVariableMonitoringResponse.json'
+import SetVariablesRequest from './../third-party/ocpp/2.0.1/SetVariablesRequest.json'
+import SetVariablesResponse from './../third-party/ocpp/2.0.1/SetVariablesResponse.json'
+import SignCertificateRequest from './../third-party/ocpp/2.0.1/SignCertificateRequest.json'
+import SignCertificateResponse from './../third-party/ocpp/2.0.1/SignCertificateResponse.json'
+import StatusNotificationRequest from './../third-party/ocpp/2.0.1/StatusNotificationRequest.json'
+import StatusNotificationResponse from './../third-party/ocpp/2.0.1/StatusNotificationResponse.json'
+import TransactionEventRequest from './../third-party/ocpp/2.0.1/TransactionEventRequest.json'
+import TransactionEventResponse from './../third-party/ocpp/2.0.1/TransactionEventResponse.json'
+import TriggerMessageRequest from './../third-party/ocpp/2.0.1/TriggerMessageRequest.json'
+import TriggerMessageResponse from './../third-party/ocpp/2.0.1/TriggerMessageResponse.json'
+import UnlockConnectorRequest from './../third-party/ocpp/2.0.1/UnlockConnectorRequest.json'
+import UnlockConnectorResponse from './../third-party/ocpp/2.0.1/UnlockConnectorResponse.json'
+import UnpublishFirmwareRequest from './../third-party/ocpp/2.0.1/UnpublishFirmwareRequest.json'
+import UnpublishFirmwareResponse from './../third-party/ocpp/2.0.1/UnpublishFirmwareResponse.json'
+import UpdateFirmwareRequest from './../third-party/ocpp/2.0.1/UpdateFirmwareRequest.json'
+import UpdateFirmwareResponse from './../third-party/ocpp/2.0.1/UpdateFirmwareResponse.json'
+
+export const jsonSchemas:{ [key: string]: any } = {
+  AuthorizeRequest,
+  AuthorizeResponse,
+  BootNotificationRequest,
+  BootNotificationResponse,
+  CancelReservationRequest,
+  CancelReservationResponse,
+  CertificateSignedRequest,
+  CertificateSignedResponse,
+  ChangeAvailabilityRequest,
+  ChangeAvailabilityResponse,
+  ClearCacheRequest,
+  ClearCacheResponse,
+  ClearChargingProfileRequest,
+  ClearChargingProfileResponse,
+  ClearDisplayMessageRequest,
+  ClearDisplayMessageResponse,
+  ClearVariableMonitoringRequest,
+  ClearVariableMonitoringResponse,
+  ClearedChargingLimitRequest,
+  ClearedChargingLimitResponse,
+  CostUpdatedRequest,
+  CostUpdatedResponse,
+  CustomerInformationRequest,
+  CustomerInformationResponse,
+  DataTransferRequest,
+  DataTransferResponse,
+  DeleteCertificateRequest,
+  DeleteCertificateResponse,
+  FirmwareStatusNotificationRequest,
+  FirmwareStatusNotificationResponse,
+  Get15118EVCertificateRequest,
+  Get15118EVCertificateResponse,
+  GetBaseReportRequest,
+  GetBaseReportResponse,
+  GetCertificateStatusRequest,
+  GetCertificateStatusResponse,
+  GetChargingProfilesRequest,
+  GetChargingProfilesResponse,
+  GetCompositeScheduleRequest,
+  GetCompositeScheduleResponse,
+  GetDisplayMessagesRequest,
+  GetDisplayMessagesResponse,
+  GetInstalledCertificateIdsRequest,
+  GetInstalledCertificateIdsResponse,
+  GetLocalListVersionRequest,
+  GetLocalListVersionResponse,
+  GetLogRequest,
+  GetLogResponse,
+  GetMonitoringReportRequest,
+  GetMonitoringReportResponse,
+  GetReportRequest,
+  GetReportResponse,
+  GetTransactionStatusRequest,
+  GetTransactionStatusResponse,
+  GetVariablesRequest,
+  GetVariablesResponse,
+  HeartbeatRequest,
+  HeartbeatResponse,
+  InstallCertificateRequest,
+  InstallCertificateResponse,
+  LogStatusNotificationRequest,
+  LogStatusNotificationResponse,
+  MeterValuesRequest,
+  MeterValuesResponse,
+  NotifyChargingLimitRequest,
+  NotifyChargingLimitResponse,
+  NotifyCustomerInformationRequest,
+  NotifyCustomerInformationResponse,
+  NotifyDisplayMessagesRequest,
+  NotifyDisplayMessagesResponse,
+  NotifyEVChargingNeedsRequest,
+  NotifyEVChargingNeedsResponse,
+  NotifyEVChargingScheduleRequest,
+  NotifyEVChargingScheduleResponse,
+  NotifyEventRequest,
+  NotifyEventResponse,
+  NotifyMonitoringReportRequest,
+  NotifyMonitoringReportResponse,
+  NotifyReportRequest,
+  NotifyReportResponse,
+  PublishFirmwareRequest,
+  PublishFirmwareResponse,
+  PublishFirmwareStatusNotificationRequest,
+  PublishFirmwareStatusNotificationResponse,
+  ReportChargingProfilesRequest,
+  ReportChargingProfilesResponse,
+  RequestStartTransactionRequest,
+  RequestStartTransactionResponse,
+  RequestStopTransactionRequest,
+  RequestStopTransactionResponse,
+  ReservationStatusUpdateRequest,
+  ReservationStatusUpdateResponse,
+  ReserveNowRequest,
+  ReserveNowResponse,
+  ResetRequest,
+  ResetResponse,
+  SecurityEventNotificationRequest,
+  SecurityEventNotificationResponse,
+  SendLocalListRequest,
+  SendLocalListResponse,
+  SetChargingProfileRequest,
+  SetChargingProfileResponse,
+  SetDisplayMessageRequest,
+  SetDisplayMessageResponse,
+  SetMonitoringBaseRequest,
+  SetMonitoringBaseResponse,
+  SetMonitoringLevelRequest,
+  SetMonitoringLevelResponse,
+  SetNetworkProfileRequest,
+  SetNetworkProfileResponse,
+  SetVariableMonitoringRequest,
+  SetVariableMonitoringResponse,
+  SetVariablesRequest,
+  SetVariablesResponse,
+  SignCertificateRequest,
+  SignCertificateResponse,
+  StatusNotificationRequest,
+  StatusNotificationResponse,
+  TransactionEventRequest,
+  TransactionEventResponse,
+  TriggerMessageRequest,
+  TriggerMessageResponse,
+  UnlockConnectorRequest,
+  UnlockConnectorResponse,
+  UnpublishFirmwareRequest,
+  UnpublishFirmwareResponse,
+  UpdateFirmwareRequest,
+  UpdateFirmwareResponse,
+}
