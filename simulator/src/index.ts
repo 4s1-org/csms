f117b7cfdec54b8951bfdd45bc8c6adc3507800c
Steffen <yellowgarbagebag@4s1.de>
2021-02-07 13:27:37 +0100

    Client connected sich

--- simulator/src/index.ts
+++ simulator/src/index.ts
@@ -1,19 +1,2 @@
-import { ChargingStation } from './charging-station'
-import { WebSocketClient } from './web-socket-client'
-
-process.on('uncaughtException', (err) => {
-  console.log(`Uncaught Exception: ${err.message}\n${err.stack}`)
-  process.exit(1)
-})
-
-process.on('unhandledRejection', (reason, promise) => {
-  console.log('Unhandled rejection at ', promise, `reason: ${reason}`)
-  process.exit(1)
-})
-
-//new WebSocketClient(new ChargingStation('LS001', 'LS001', 'test'), new WebSocket('')).run()
-//new WebSocketClient('LS002').run()
-
 export { ChargingStation } from './charging-station'
 export { WebSocketClient } from './web-socket-client'
-export { IWebSocket } from './i-websocket'
