694e6f02d9375345c8afde1f9007c142f2f3265e
Steffen <steffen@4s1.de>
2021-02-09 18:48:28 +0100

    Buildorder

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 pnpm install
-pnpm -r exec pnpm run build
+pnpm run build --prefix common-lib
+pnpm run build --prefix ocpp-lib
+
+pnpm run build --prefix backend
+pnpm run build --prefix backend-admin-ui
+
+pnpm run build --prefix charging-station-lib
+pnpm run build --prefix charging-station-ui
+pnpm run build --prefix charging-station-cli
+
