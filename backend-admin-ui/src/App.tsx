3fbbf5fc0fe195722a01027b61592d9d532b58c7
Steffen <yellowgarbagebag@4s1.de>
2021-02-09 19:52:52 +0100

    Ladesäulen werden angezeigt

--- backend-admin-ui/src/App.tsx
+++ backend-admin-ui/src/App.tsx
@@ -1,29 +1,16 @@
 import React from 'react'
+import AdminComp from './admin'
+
 import './App.css'
 
 function App() {
-  var authToken = 'YWRtaW46YWRtaW4=' // admin:admin
-  document.cookie = 'X-Authorization=' + authToken + '; path=/'
 
-  const ws = new WebSocket('wss://localhost:3000/admin', ['ocpp2.0.1'])
 
-  ws.onopen = () => {
-    console.log('IT WORKS')
-  }
-  ws.onmessage = (msg: any): void => {}
-  ws.onerror = (msg: any): void => {}
-  ws.onclose = (): void => {}
+
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <AdminComp />
     </div>
   )
 }
