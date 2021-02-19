c86098805904ee2597252956cea8227c49a09303
Steffen <yellowgarbagebag@4s1.de>
2021-02-19 15:45:32 +0100

    Model von der CS wird auch im Server genutzt

--- csms-server/create-cs.sh
+++ csms-server/create-cs.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-for i in $(seq -f "%03g" 1 20)
+for i in $(seq -f "%03g" 1 11)
 do
   echo '{"username":"LS'$i'","password":"test","state":"Offline","uniqueIdentifier":"LS'$i'"}' > data/LS$i.json
 done
