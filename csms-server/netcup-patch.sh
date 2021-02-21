b6ce9de11a385e21ab0916f1daf2d5f4dc548201
Steffen <yellowgarbagebag@4s1.de>
2021-02-21 13:59:09 +0100

    publish config

---
+++ csms-server/netcup-patch.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sed -i 's/"@yellowgarbagebag\/common-lib": "workspace:\*"/"@yellowgarbagebag\/ocpp-lib": "file:..\/yellowgarbagebag-common-lib.tgz"/g' package.json
+sed -i 's/"@yellowgarbagebag\/csms-lib": "workspace:\*"/"@yellowgarbagebag\/ocpp-lib": "file:..\/yellowgarbagebag-csms-lib.tgz"/g' package.json
+sed -i 's/"@yellowgarbagebag\/ocpp-lib": "workspace:\*"/"@yellowgarbagebag\/ocpp-lib": "file:..\/yellowgarbagebag-ocpp-lib.tgz"/g' package.json
