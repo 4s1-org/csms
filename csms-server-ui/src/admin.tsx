7c17729c031ffed72a625fb5c1e0d634ef1b08bf
Steffen <steffen@4s1.de>
2021-02-21 20:13:17 +0100

    Login geschaffen

--- csms-server-ui/src/admin.tsx
+++ csms-server-ui/src/admin.tsx
@@ -4,6 +4,9 @@ import { ChargingStationModel, SerializationHelper } from '@yellowgarbagebag/csm
 import './admin.css'
 interface IState {
   data: ChargingStationModel[]
+  server: string
+  username: string
+  password: string
 }
 
 interface IProps {}
@@ -13,20 +16,23 @@ class AdminComp extends React.Component<IProps, IState> {
     super(props)
     this.state = {
       data: [],
+      server: 'localhost:3000',
+      username: 'admin',
+      password: '',
     }
+
+    this.onBtnLoginClick = this.onBtnLoginClick.bind(this)
+
+    this.onEdtServerChange = this.onEdtServerChange.bind(this)
+    this.onEdtUsernameChange = this.onEdtUsernameChange.bind(this)
+    this.onEdtPasswordChange = this.onEdtPasswordChange.bind(this)
   }
 
-  public async componentDidMount(): Promise<void> {
-    await this.loadData()
-  }
-
-  public async componentDidUpdate(prevProps: IProps): Promise<void> {}
-
-  private async loadData(): Promise<void> {
-    const authToken = 'YWRtaW46YWRtaW4=' // admin:admin
+  private async login(): Promise<void> {
+    var authToken = window.btoa(`${this.state.username}:${this.state.password}`)
     document.cookie = 'X-Authorization=' + authToken + '; path=/'
 
-    const ws = new WebSocket('wss://localhost:3000/admin', ['ocpp2.0.1'])
+    const ws = new WebSocket(`wss://${this.state.server}/admin`, ['ocpp2.0.1'])
 
     ws.onopen = () => {
       console.log('IT WORKS')
@@ -43,14 +49,82 @@ class AdminComp extends React.Component<IProps, IState> {
 
   public render(): JSX.Element {
     return (
-      <div className="container-fluid">
-        <div className="row">
-          {this.state.data.map((item) => (
-            <ChargingStationComp key={item.uniqueIdentifier} data={item}></ChargingStationComp>
-          ))}
+      <div>
+        <nav className="navbar navbar-dark bg-dark flex-column flex-md-row">
+          <form className="form-inline my-2 my-lg-0">
+            <input
+              className="form-control mr-sm-2"
+              placeholder="Server"
+              aria-label="Server"
+              type="text"
+              onChange={this.onEdtServerChange}
+              value={this.state.server}
+            />
+            <input
+              className="form-control mr-sm-2"
+              placeholder="Username"
+              aria-label="Username"
+              type="text"
+              onChange={this.onEdtUsernameChange}
+              value={this.state.username}
+            />
+            <input
+              className="form-control mr-sm-2"
+              placeholder="Password"
+              aria-label="Password"
+              type="password"
+              onChange={this.onEdtPasswordChange}
+              value={this.state.password}
+            />
+            <button className="btn mr-sm-2 bg-light" type="submit" onClick={this.onBtnLoginClick}>
+              Login
+            </button>
+            <button className="btn mr-sm-2 bg-light" type="submit" onClick={this.onBtnLogoutClick}>
+              Logout
+            </button>
+          </form>
+        </nav>
+
+        <div className="container-fluid">
+          <div className="row">
+            {this.state.data.map((item) => (
+              <ChargingStationComp key={item.uniqueIdentifier} data={item}></ChargingStationComp>
+            ))}
+          </div>
         </div>
       </div>
     )
+  }
+
+  private onBtnLoginClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+    e.preventDefault()
+    this.login()
+  }
+
+  private onBtnLogoutClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+    e.preventDefault()
+    this.login()
+  }
+
+  private onEdtServerChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    this.setState({
+      ...this.state,
+      server: e.currentTarget.value,
+    })
+  }
+
+  private onEdtUsernameChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    this.setState({
+      ...this.state,
+      username: e.currentTarget.value,
+    })
+  }
+
+  private onEdtPasswordChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    this.setState({
+      ...this.state,
+      password: e.currentTarget.value,
+    })
   }
 }
 
