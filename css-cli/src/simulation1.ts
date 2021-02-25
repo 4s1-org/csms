2c4ad7075b4bd626ed2ec754ee81f134a199fb52
Steffen <steffen@4s1.de>
2021-02-25 12:24:48 +0100

    CSS Cli nutzt neues Pattern

--- css-cli/src/simulation1.ts
+++ css-cli/src/simulation1.ts
@@ -1,64 +1,41 @@
-import { ChargingStation, WebSocketClient } from '@yellowgarbagebag/css-lib'
-import WebSocket from 'ws'
+import { SimulationBase } from './simulation-base'
 
-const cs = new ChargingStation('LS001', 'LS001', 'test')
-const socket = new WebSocket(`wss://localhost:3000/ocpp/${cs.uniqueIdentifier}`, ['ocpp2.0.1'], {
-  headers: {
-    authorization: `Basic ${Buffer.from(`${cs.username}:${cs.password}`).toString('base64')}`,
-  },
-})
-
-const sendCallback = (msg: any): boolean => {
-  if (socket && socket.OPEN) {
-    socket.send(msg)
-    return true
+class Simulation extends SimulationBase {
+  constructor() {
+    super('LS001', 'LS001', 'test')
   }
-  return false
-}
-const client = new WebSocketClient(cs, sendCallback)
 
-socket.onopen = (): void => {
-  cs.connect()
+  protected simulate(): void {
+    this.css.send(this.css.sendBootNotificationRequest())
 
-  client.send(cs.sendBootNotificationRequest())
+    setTimeout(() => {
+      this.css.send(this.css.sendStatusNotificationRequest())
+    }, 100)
 
-  setTimeout(() => {
-    client.send(cs.sendStatusNotificationRequest())
-  }, 100)
+    setTimeout(() => {
+      this.css.send(this.css.sendAuthorizationRequest_PinCode())
+    }, 200)
 
-  setTimeout(() => {
-    client.send(cs.sendAuthorizationRequest_PinCode())
-  }, 200)
+    setTimeout(() => {
+      this.css.send(this.css.sendAuthorizationRequest_Rfid())
+    }, 300)
 
-  setTimeout(() => {
-    client.send(cs.sendAuthorizationRequest_Rfid())
-  }, 300)
+    setTimeout(() => {
+      this.css.send(this.css.sendMeterValueRequest())
+    }, 400)
 
-  setTimeout(() => {
-    client.send(cs.sendMeterValueRequest())
-  }, 400)
+    setTimeout(() => {
+      this.css.send(this.css.sendHeartbeatRequest())
+    }, 500)
 
-  setTimeout(() => {
-    client.send(cs.sendHeartbeatRequest())
-  }, 500)
+    setTimeout(() => {
+      this.css.send(this.css.sendNotifyEventRequest_LockFailure())
+    }, 600)
 
-  setTimeout(() => {
-    client.send(cs.sendNotifyEventRequest_LockFailure())
-  }, 600)
-
-  setTimeout(() => {
-    socket.close()
-  }, 2000)
+    setTimeout(() => {
+      this.css.disconnect()
+    }, 2000)
+  }
 }
 
-socket.onmessage = (msg: WebSocket.MessageEvent): void => {
-  client.onMessage(msg.data)
-}
-
-socket.onerror = (err: WebSocket.ErrorEvent): void => {
-  client.onError(err.message)
-}
-
-socket.onclose = (): void => {
-  client.onClose()
-}
+new Simulation()
