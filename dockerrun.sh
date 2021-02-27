9a4cbc168c048b3222152e25755c5b80aaa06d9d
Steffen <yellowgarbagebag@4s1.de>
2021-02-27 21:57:53 +0100

    Docker läuft

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 
 docker run -it \
-  -p 5000:5000 \
   -p 3000:3000 \
   -p 3001:3001 \
   -p 3002:3002 \
-  csms \
-  sh
+  csms
+
+# docker build -t csms .
+# docker exec -it csms sh
