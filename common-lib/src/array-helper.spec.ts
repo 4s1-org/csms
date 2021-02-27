11b0c3a6e6f1675c547621cbdb6d7fa15ce9a1aa
Steffen <yellowgarbagebag@4s1.de>
2021-02-27 18:51:02 +0100

    Fehler bei nicht vorhandenem Item gefixt

--- common-lib/src/array-helper.spec.ts
+++ common-lib/src/array-helper.spec.ts
@@ -14,7 +14,7 @@ describe('Array helper', () => {
       expect(arr).toEqual(expect.arrayContaining([item]))
     })
 
-    it('No item match', () => {
+    it('No item match, add it', () => {
       // Arrange
       let arr = [{ id: '4', val: 'foo' }]
       const item = { id: '1', val: 'bar' }
@@ -22,8 +22,8 @@ describe('Array helper', () => {
       arr = updateItemInArray(arr, item, 'id')
       // Assert
       expect(arr).toBeDefined()
-      expect(arr).toHaveLength(1)
-      expect(arr).toEqual(expect.arrayContaining([{ id: '4', val: 'foo' }]))
+      expect(arr).toHaveLength(2)
+      expect(arr).toEqual(expect.arrayContaining([item]))
     })
   })
 })
--- common-lib/src/array-helper.ts
+++ common-lib/src/array-helper.ts
@@ -3,6 +3,7 @@ export function updateItemInArray<T>(arr: T[], item: T, keyField: keyof T): T[] 
   if (idx !== -1) {
     return [...arr.slice(0, idx), Object.assign({}, arr[idx], item), ...arr.slice(idx + 1)]
   } else {
+    arr.push(item)
     return arr
   }
 }
