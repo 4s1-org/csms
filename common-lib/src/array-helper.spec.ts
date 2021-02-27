a59f30e94259c00dbe6f601e673562d46cbb9800
Steffen <steffen@4s1.de>
2021-02-27 18:23:54 +0100

    Added tests and keyfield

---
+++ common-lib/src/array-helper.spec.ts
@@ -0,0 +1,29 @@
+import { updateItemInArray } from './array-helper'
+
+describe('Array helper', () => {
+  describe('updateItemInArray()', () => {
+    it('Update item', () => {
+      // Arrange
+      let arr = [{ id: '1', val: 'foo' }]
+      const item = { id: '1', val: 'bar' }
+      // Act
+      arr = updateItemInArray(arr, item, 'id')
+      // Assert
+      expect(arr).toBeDefined()
+      expect(arr).toHaveLength(1)
+      expect(arr).toEqual(expect.arrayContaining([item]))
+    })
+
+    it('No item match', () => {
+      // Arrange
+      let arr = [{ id: '4', val: 'foo' }]
+      const item = { id: '1', val: 'bar' }
+      // Act
+      arr = updateItemInArray(arr, item, 'id')
+      // Assert
+      expect(arr).toBeDefined()
+      expect(arr).toHaveLength(1)
+      expect(arr).toEqual(expect.arrayContaining([{ id: '4', val: 'foo' }]))
+    })
+  })
+})
--- common-lib/src/array-helper.ts
+++ common-lib/src/array-helper.ts
@@ -1,5 +1,5 @@
-export function updateItemInArray<T extends { id: string }>(arr: T[], id: string, item: T): T[] {
-  const idx = arr.findIndex((x) => x.id === id)
+export function updateItemInArray<T>(arr: T[], item: T, keyField: keyof T): T[] {
+  const idx = arr.findIndex((x) => x[keyField] === item[keyField])
   if (idx !== -1) {
     return [...arr.slice(0, idx), Object.assign({}, arr[idx], item), ...arr.slice(idx + 1)]
   } else {
