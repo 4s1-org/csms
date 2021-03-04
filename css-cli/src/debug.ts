17198379f427a19df4cd4ee48f8f7fc6c79d3288
Steffen <steffen@4s1.de>
2021-03-04 12:34:45 +0100

    Simulator empfängt auch Nachrichten

--- css-cli/src/debug.ts
+++ css-cli/src/debug.ts
@@ -1,25 +1,38 @@
-import { ChargingStation } from '@yellowgarbagebag/css-lib/dist/charging-station'
-import { MessageInfoDto } from '@yellowgarbagebag/ocpp-lib'
-import { sleep } from '../../css-lib/node_modules/@yellowgarbagebag/common-lib/dist'
+import {
+  CsmsError,
+  GetVariablesRequestDto,
+  OcppErrorCodeEnum,
+  RequestBaseDto,
+  ResponseBaseDto,
+  SetVariablesRequestDto,
+} from '@yellowgarbagebag/ocpp-lib'
+import { ChargingStation } from './charging-station'
 import { WsClient } from './ws-client'
 
-class ChargingStation2 extends ChargingStation {}
-
 class Simulation {
+  private cs: ChargingStation
   private client: WsClient
-  private cs: ChargingStation2
 
   constructor() {
-    this.client = new WsClient('LS001', 'LS001', 'test')
-    this.cs = new ChargingStation2('LS001')
-    console.log('constr')
+    this.cs = new ChargingStation('LS001')
+    this.client = new WsClient((payload: RequestBaseDto): ResponseBaseDto => this.onMessage(payload))
   }
 
   public async simulate(): Promise<void> {
-    console.log('simulate')
-    await sleep(2000)
+    await this.client.connect('LS001', 'LS001', 'test')
     const response = await this.client.send(this.cs.sendBootNotificationRequest())
     console.log(response)
+
+    // this.client.disconnect()
+  }
+
+  private onMessage(payload: RequestBaseDto): ResponseBaseDto {
+    if (payload instanceof GetVariablesRequestDto) {
+      return this.cs.receiveGetVariablesRequest(payload)
+    }
+
+    // ToDo: Hier darf ich dem Server eine Fehlermeldung schicken
+    throw new CsmsError(OcppErrorCodeEnum.NotSupported)
   }
 }
 
