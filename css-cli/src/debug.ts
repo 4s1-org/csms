f217f1e12a801a87c31977df28f52db2c2fbddae
Steffen <yellowgarbagebag@4s1.de>
2021-03-04 11:41:28 +0100

    Erster Wurf mit Promise

--- css-cli/src/debug.ts
+++ css-cli/src/debug.ts
@@ -1,13 +1,31 @@
-import { SimulationBase } from './simulation-base'
+import { ChargingStation } from '@yellowgarbagebag/css-lib/dist/charging-station'
+import { MessageInfoDto } from '@yellowgarbagebag/ocpp-lib'
+import { sleep } from '../../css-lib/node_modules/@yellowgarbagebag/common-lib/dist'
+import { WsClient } from './ws-client'
 
-class Simulation extends SimulationBase {
+class ChargingStation2 extends ChargingStation {}
+
+class Simulation {
+  private client: WsClient
+  private cs: ChargingStation2
+
   constructor() {
-    super('LS001', 'LS001', 'test')
+    this.client = new WsClient('LS001', 'LS001', 'test')
+    this.cs = new ChargingStation2('LS001')
+    console.log('constr')
   }
 
-  protected simulate(): void {
-    this.css.send(this.css.sendAuthorizationRequest_Rfid())
+  public async simulate(): Promise<void> {
+    console.log('simulate')
+    await sleep(2000)
+    const response = await this.client.send(this.cs.sendBootNotificationRequest())
+    console.log(response)
   }
 }
 
-new Simulation()
+async function main(): Promise<void> {
+  const sim = new Simulation()
+  await sim.simulate()
+}
+
+main().catch(console.error)
