edb32603f67ae49372e58e5ccaea9d718ea0e415
Steffen <yellowgarbagebag@4s1.de>
2021-03-08 15:48:01 +0000

    Typo

--- csms-server-ui/src/components/charging-station.tsx
+++ csms-server-ui/src/components/charging-station.tsx
@@ -37,7 +37,7 @@ class ChargingStationComp extends React.Component<IProps, IState> {
         <h5 className={'card-header ' + headerColorCss}>{this.props.data.uniqueIdentifier}</h5>
         <div className="card-body">
           <h6 className="card-subtitle mb-2">Status: {stateStr}</h6>
-          <p className="card-text">Benutername: {this.props.data.username}</p>
+          <p className="card-text">Benutzername: {this.props.data.username}</p>
         </div>
         <div className="card-footer">
           <small className="text-muted">Letztes Kommando: {this.props.data.lastCommand}</small>
