34b66366bb7c490d247610ddc7bfb10fa76593ae
Steffen <yellowgarbagebag@4s1.de>
2021-03-21 15:08:41 +0100

    shutdown handling

--- csms-server/src/index.ts
+++ csms-server/src/index.ts
@@ -3,7 +3,7 @@ import { DataStorage } from './config/data-storage'
 import { IDataStorageSchema } from './config/i-data-store-schema'
 import { WebSocketServer } from './web-socket-server'
 
-const logger = new Logger('root')
+const logger = new Logger('Core')
 
 // Config
 const dataStorage = new DataStorage<IDataStorageSchema>('csms-server')
@@ -18,14 +18,15 @@ if (!dataStorage.has('chargingStationModels')) {
 const server = new WebSocketServer(dataStorage)
 server.startServer()
 
-async function closeGracefully(signal: number): Promise<void> {
+function closeGracefully(signal: string): Promise<void> {
   logger.warn(`Received signal to terminate: ${signal}`)
   server.stopServer()
+  logger.info('gracefull shutdown successfully')
   process.exit()
 }
 
-process.on('SIGINT', closeGracefully)
-process.on('SIGTERM', closeGracefully)
+process.on('SIGINT', (signal: string) => closeGracefully(signal))
+process.on('SIGTERM', (signal: string) => closeGracefully(signal))
 
 process.on('uncaughtException', (err) => {
   logger.error(`Uncaught Exception: ${err.message}`)
