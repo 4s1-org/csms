5822c3bd4085739f23e2df2ce7e65dc84d4aee7c
Steffen <yellowgarbagebag@4s1.de>
2021-03-24 19:00:14 +0100

    Deserialisierung verbessert

--- csms-lib/src/charging-station-model.ts
+++ csms-lib/src/charging-station-model.ts
@@ -1,4 +1,4 @@
-import { Expose } from 'class-transformer'
+import { Expose, Transform, Type } from 'class-transformer'
 
 export enum ChargingStationGroupFlag {
   ServerOnly = 'serverOnly',
@@ -11,39 +11,53 @@ export enum ChargingStationState {
   Online,
 }
 
+export class Evse {
+  public constructor(public readonly evseId: number, public readonly user: string) {
+    // nothing to do
+  }
+}
+
 export class ChargingStationModel {
   @Expose({ name: '_username' })
+  @Transform(({ value }) => value || '', { toClassOnly: true })
   public username = ''
 
   /**
    * Only at Server
    */
   @Expose({ name: '_passwordHash', groups: [ChargingStationGroupFlag.ServerOnly] })
+  @Transform(({ value }) => value || '', { toClassOnly: true })
   public passwordHash = ''
 
   /**
    * Only at UI
    */
   @Expose({ name: '_state', groups: [ChargingStationGroupFlag.UiOnly] })
+  @Transform(({ value }) => value || ChargingStationState.Offline, { toClassOnly: true })
   public state = ChargingStationState.Offline
 
   @Expose({ name: '_uniqueIdentifier' })
-  public readonly uniqueIdentifier: string
+  @Transform(({ value }) => value || '', { toClassOnly: true })
+  public readonly uniqueIdentifier: string = ''
 
   @Expose({ name: '_lastContact' })
+  @Transform(({ value }) => value || 0, { toClassOnly: true })
   public lastContact = 0
 
   /**
    * Only at UI
    */
   @Expose({ name: '_lastCommand', groups: [ChargingStationGroupFlag.UiOnly] })
+  @Transform(({ value }) => value || '', { toClassOnly: true })
   public lastCommand = ''
 
   /**
    * Only at UI
    */
   @Expose({ name: '_evse', groups: [ChargingStationGroupFlag.UiOnly] })
-  public evse: { evseId: number; user: string }[] = []
+  @Transform(({ value }) => value || [], { toClassOnly: true })
+  @Type(() => Evse)
+  public evse: Evse[] = []
 
   public constructor(uniqueIdentifier: string) {
     this.uniqueIdentifier = uniqueIdentifier
