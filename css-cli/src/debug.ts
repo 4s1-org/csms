cef8159af7e2d064bd7b0a9d30817e650eea19e2
Steffen <yellowgarbagebag@4s1.de>
2021-04-03 18:52:21 +0200

    .env

--- css-cli/src/debug.ts
+++ css-cli/src/debug.ts
@@ -1,18 +1,14 @@
 import { sleep } from '@yellowgarbagebag/common-lib'
-import { ChargingStation } from '@yellowgarbagebag/css-lib'
-import { WsClient } from './ws-client'
+import { SimulationBase } from './simulation-base'
 
-class Simulation {
-  private cs: ChargingStation
-  private client: WsClient
-
+class Simulation extends SimulationBase {
   constructor() {
-    this.client = new WsClient('LS001')
-    this.cs = new ChargingStation('LS001', this.client)
+    super()
   }
 
   public async simulate(): Promise<void> {
-    await this.client.connect(this.cs, 'LS001', 'test')
+    await this.connect()
+
     await this.cs.sendBootNotificationRequest()
     await sleep(200)
     await this.cs.sendAuthorizationRequest_PinCode()
@@ -31,9 +27,4 @@ class Simulation {
   }
 }
 
-async function main(): Promise<void> {
-  const sim = new Simulation()
-  await sim.simulate()
-}
-
-main().catch(console.error)
+new Simulation().simulate()
