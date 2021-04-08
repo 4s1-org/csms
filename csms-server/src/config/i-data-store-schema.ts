45ef910dd54816ccccf380eb7fa5878aadf2a028
Steffen <yellowgarbagebag@4s1.de>
2021-04-08 17:07:10 +0200

    Benutzer werden in der UI angezeigt

--- csms-server/src/config/i-data-store-schema.ts
+++ csms-server/src/config/i-data-store-schema.ts
@@ -1,9 +1,14 @@
+export interface IValidUser {
+  name: string
+  rfid: string
+}
+
 export interface IDataStorageSchema {
   port: number
   adminCredentials: {
     username: string
     passwordHash: string
   }
-  validUsers: Array<{ name: string; rfid: string }>
+  validUsers: IValidUser[]
   chargingStationModels: string[]
 }
