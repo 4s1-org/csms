4d9d2082edf765d77b7d0835c54059cd133900ca
Steffen <yellowgarbagebag@4s1.de>
2021-04-22 10:14:08 +0200

    Neue Unterseiten angelegt

---
+++ csms-server-ui/src/components/stations/main-stations.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface IState {}
+interface IProps {}
+
+class MainStations extends React.Component<IProps, IState> {
+  public render(): JSX.Element {
+    return <div>MainStations</div>
+  }
+}
+
+export default MainStations
