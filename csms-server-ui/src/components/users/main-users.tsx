8134ed04062af32419e875a0a7276c99bdeb4045
Steffen <steffen@4s1.de>
2021-04-22 10:14:08 +0200

    Neue Unterseiten angelegt

---
+++ csms-server-ui/src/components/users/main-users.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface IState {}
+interface IProps {}
+
+class MainUsers extends React.Component<IProps, IState> {
+  public render(): JSX.Element {
+    return <div>MainUsers</div>
+  }
+}
+
+export default MainUsers
