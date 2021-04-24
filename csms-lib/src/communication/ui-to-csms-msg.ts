926bdb92cb6269440d349ea4bf2e8aac6d3641dd
Steffen <steffen@4s1.de>
2021-04-24 16:55:11 +0200

    Kommentare

--- csms-lib/src/communication/ui-to-csms-msg.ts
+++ csms-lib/src/communication/ui-to-csms-msg.ts
@@ -4,6 +4,9 @@ import { UiToCsmsCmdEnum } from './ui-to-csms-cmd.enum'
 import { UiToCsmsCsSubCmdEnum } from './ui-to-csms-cs-sub-cmd.enum'
 import { UiToCsmsUserSubCmdEnum } from './ui-to-csms-user-sub-cmd.enum'
 
+/**
+ * Message which can be send from UI to the CSMS.
+ */
 export class UiToCsmsMsg {
   constructor(
     public readonly cmd: UiToCsmsCmdEnum,
