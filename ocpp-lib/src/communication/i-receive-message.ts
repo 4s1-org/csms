2b2ff71b5bb9205c30efb569c5c06b4f8670698a
Steffen <steffen@4s1.de>
2021-04-24 16:59:06 +0200

    Kommentare

--- ocpp-lib/src/communication/i-receive-message.ts
+++ ocpp-lib/src/communication/i-receive-message.ts
@@ -2,6 +2,9 @@ import { OcppActionEnum } from '../generated/ocpp-action.enum'
 import { RequestBaseDto } from '../generated/request-base.dto'
 import { ResponseBaseDto } from '../generated/response-base.dto'
 
+/**
+ * Interface for the receive-function.
+ */
 export interface IReceiveMessage {
   receive: (payload: RequestBaseDto, action: OcppActionEnum) => ResponseBaseDto
 }
