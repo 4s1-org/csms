a0f6291a2fbfbb0a83eea01cee9a7ddb82033a23
Steffen <yellowgarbagebag@4s1.de>
2021-05-04 19:03:10 +0200

    rename

---
+++ csms-lib/src/datastructs/chipcard.model.ts
@@ -0,0 +1,10 @@
+/**
+ *  Model for a chipcard
+ */
+export class ChipcardModel {
+  public id = ''
+  public rfid = ''
+  public description = ''
+  public costCenter = ''
+  public enabled = false
+}
