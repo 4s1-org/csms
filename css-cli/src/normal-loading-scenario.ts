ad19761e7e347f6b7918361b77e0e0ae11a5465e
Steffen <steffen@4s1.de>
2021-05-11 20:46:40 +0200

    Pentest added

--- css-cli/src/normal-loading-scenario.ts
+++ css-cli/src/normal-loading-scenario.ts
@@ -25,11 +25,7 @@ import { SimulationBase } from './simulation-base'
 /**
  *
  */
-class Simulation1 extends SimulationBase {
-  constructor() {
-    super()
-  }
-
+export class NormalLoadingScenario extends SimulationBase {
   public async simulate(): Promise<void> {
     let seqNo = 0
     const transactionId = `x${Date.now()}`
@@ -291,4 +287,4 @@ class Simulation1 extends SimulationBase {
   }
 }
 
-new Simulation1().simulate()
+new NormalLoadingScenario().simulate()
