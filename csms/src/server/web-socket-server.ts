da3976d77e1084f42dd862523dedf128b24f9f26
Steffen <steffen@4s1.de>
2021-09-26 20:05:26 +0000

    Update web-socket-server.ts

--- csms/src/server/web-socket-server.ts
+++ csms/src/server/web-socket-server.ts
@@ -72,12 +72,12 @@ export class WebSocketServer {
     // Create a WebSocket server for charging station connections.
     this.wssChargingStations = new WebSocket.Server({
       noServer: true,
-      handleProtocols: (protocols: Set<string>, request: http.IncomingMessage): string | boolean => {
+      handleProtocols: (protocols: Set<string>, request: http.IncomingMessage): string => {
         // Check ocpp 2.0.1 protocol
         if (!protocols.has(Const.ocppProtocolName)) {
           this.logger.warn(`Missing OCPP 2.0.1 protocol from ${request.url}`)
           request.destroy()
-          return false
+          return
         }
         // Send back the protocol, which should be used.
         return Const.ocppProtocolName
